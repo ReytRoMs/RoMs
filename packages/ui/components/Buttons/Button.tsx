4e7419522aaecdcdcc05611a784d67803d0276a9
Dan Stevenson <dan.stevenson@reyt.co.uk>
2024-02-23 15:30:49 +0000

    BEST-370 update Button props

--- packages/ui/components/Buttons/Button.tsx
+++ packages/ui/components/Buttons/Button.tsx
@@ -1,5 +1,4 @@
 import { Button as GSButton, ButtonText as GSButtonText } from "@gluestack-ui/themed";
-import { ComponentProps } from "react";
 import { Clipboard } from "../Icons";
 
 type ButtonVariant = "solid" | "outline" | "link";
@@ -7,10 +6,8 @@ type ButtonVariant = "solid" | "outline" | "link";
 interface IButton {
 	variant: ButtonVariant;
 	buttonText: string;
-	// iconName?: IconsName;
 	isDisabled?: boolean;
 	onPress?: () => void;
-	borderColour?: ComponentProps<typeof GSButton>["borderColor"];
 }
 
 export const Button = ({ buttonText, isDisabled = false, variant, onPress }: IButton) => {

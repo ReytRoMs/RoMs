1dc785bc3d2ac4d5d659c36c832f1c56ecc38224
Dan Stevenson <dan.stevenson@reyt.co.uk>
2024-02-23 15:13:52 +0000

    BEST-370 add unstyled button to home screen with clipboard icon

--- apps/exam-webapp/app/page.tsx
+++ apps/exam-webapp/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Box, Text, View } from "@gluestack-ui/themed";
+import { Box } from "@gluestack-ui/themed";
+import { Button } from "../../../packages/ui/components";
 
 export default function Home() {
 	return (
@@ -15,7 +16,7 @@ const Container = () => {
 		// @ts-ignore
 		<Box flex={1} backgroundColor='$background' $web-h={"100vh"}>
 			<Box flex={1} $base-my={"$16"} $base-mx={"$5"} $lg-my={"$24"} $lg-mx={"$32"} alignItems='center'>
-				<Text>Hello</Text>
+				<Button buttonText='Start' variant='solid'></Button>
 			</Box>
 		</Box>
 	);

5654e94263d382e8e0061940395a22faa0c2626c
Dan Stevenson <dan.stevenson@reyt.co.uk>
2024-02-28 17:14:27 +0000

    BEST-372 add linting config

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,9 +1,13 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
 	root: true,
-	extends: ["@repo/eslint-config/react-internal.js"],
+	extends: ["@repo/eslint-config/react-internal.js", "eslint:recommended", "plugin:@typescript-eslint/recommended"],
 	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
 	rules: {
-		"no-console": ["warn", { allow: ["error"] }]
+		"@typescript-eslint/ban-ts-comment": "off",
+		"no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": ["error"],
+		"no-console": ["error", { allow: ["warn", "error"] }]
 	}
 };

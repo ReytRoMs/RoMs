4c7f29bd4a2e5f3b65e12b7f9f4b2cdc23f94ae2
Jordan Chesham <jordan.chesham@reyt.co.uk>
2024-02-28 22:16:45 +0000

    Installed SWR for CSR

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,3 +2,8 @@ export * from "./components";
 export * from "./screens";
 export { default as StyledRegistry } from "./config/registry";
 export { Providers } from "./config/providers";
+
+export const fetcher = (url: string) =>
+	fetch(url)
+		.then((res) => res.json())
+		.catch((err) => console.error(err));

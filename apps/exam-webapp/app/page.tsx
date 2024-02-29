cee132f7a6f133edec2db248d240b00a6ec0809b
Dan Stevenson <dan.stevenson@reyt.co.uk>
2024-02-29 16:09:14 +0000

    BEST-376 remove dummy data from page

--- apps/exam-webapp/app/page.tsx
+++ apps/exam-webapp/app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { Box, Text } from "@gluestack-ui/themed";
+import { Box } from "@gluestack-ui/themed";
 import { Button, ButtonVariant } from "../../../packages/ui/components";
-import useSWR from "swr";
-import { fetcher } from "ui";
 
 export default function Home() {
 	return (
@@ -14,16 +12,10 @@ export default function Home() {
 }
 
 const Container = () => {
-	const { data: user } = useSWR("/api/user", fetcher);
-	const { data: video } = useSWR("/api/video", fetcher);
-
 	return (
 		// @ts-expect-error web-h doesn't seem to exists in BoxProps
 		<Box flex={1} backgroundColor='$background' $web-h={"100vh"}>
 			<Box flex={1} $base-my={"$16"} $base-mx={"$5"} $lg-my={"$24"} $lg-mx={"$32"} alignItems='center'>
-				<Text>{`Hello ${user?.UsersPrimaryRole ?? "you"}!`}</Text>
-				<Text mb='$16'>{`First video ID: ${video?.youtube_id ?? "loading..."}`}</Text>
-
 				<Button buttonText='Primary' variant={ButtonVariant.PRIMARY}></Button>
 				<Button buttonText='Primary' variant={ButtonVariant.PRIMARY} isDisabled></Button>
 

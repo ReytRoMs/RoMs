59a00e97a383f9000ff03de1bcf55c2539a8dda9
Dan Stevenson <dan.stevenson@reyt.co.uk>
2024-02-29 10:19:32 +0000

    BEST-378 remove unused config

--- apps/exam-webapp/app/api/videos/route.ts
+++ apps/exam-webapp/app/api/videos/route.ts
@@ -1,8 +1,5 @@
 import { NextResponse } from "next/server";
-
 import { get } from "@vercel/edge-config";
-
-export const config = { matcher: "/welcome" };
 
 export async function GET() {
 	const videos = await get("videos");

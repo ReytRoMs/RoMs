a462ef402b7bcd330c4cd8835e2998294fcbba71
Dan Stevenson <dan.stevenson@reyt.co.uk>
2024-03-04 09:58:12 +0000

    BEST-376 remove unused

--- apps/exam-webapp/types/index.ts
+++ apps/exam-webapp/types/index.ts
@@ -1,10 +1,3 @@
-import { UsersPrimaryRole } from "@prisma/client";
-
-export type NewSessionUserRequest = {
-	isCurrentRomsMember: boolean;
-	usersPrimaryRole: UsersPrimaryRole;
-};
-
 export type VideoData = {
 	youtube_id: string;
 	correct_answer?: string;
